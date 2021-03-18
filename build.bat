0ab00d3b4ebf3e5a83b95813a489fce5d7704b03
nichnet <19955751+nichnet@users.noreply.github.com>
2021-03-18 18:48:11 +1030

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,3 @@
+..\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o build/bin/main.o files/main.c
+..\gbdk\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o build/main.gb build/bin/main.o
+pause
